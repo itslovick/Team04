640f2026dbfb2da6eda21fe82634f12abc1bbf8d
itslovick <superman23504@LovickStephen.local>
2015-10-14 23:38:25 -0500

    Interface Added
    Membership Interface: Added

---
+++ Membership/membershipTypes/Membership.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package membershipTypes;
+
+/**
+ *
+ * @author Abhi
+ */
+public interface Membership
+{
+    /*
+     * 
+     */
+	int getCheckOut();
+    /*
+     * 
+     */
+	int getCost();
+    /*
+     * 
+     */
+	String getDueDate();
+    /*
+     * 
+     */
+	String getBasket();
+            
+}
