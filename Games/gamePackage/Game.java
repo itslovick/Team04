78734452e98abe52d670557410f0742cb60d8da1
Kristian Saenz <kristian.saenz49@gmail.com>
2015-10-19 21:42:47 -0500

    Worked on game class

--- Games/gamePackage/Game.java
+++ Games/gamePackage/Game.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gamePackage;
+package gamestop;
 
 /**
  *
@@ -11,19 +11,65 @@ package gamePackage;
  */
 public class Game 
 {
+    //----- F I E L D S -----------------//----------------
+    
     public String title;
     public String genre;
     public int quantity;
     public int id_Num;
     
+    //==================== G E N R E S ==================//
+    
+    // Adventure array.
+    String adventure[] = { "Uncharted", "Far Cry 4", "Black Island" };
+    int adventCopies[] = {10, 10, 10};
+    
+    // Action array.
+    String action[] = { "Bioshock 2", "Assasin's Creed", "Gears of War" };
+    int actionCopies[] = {10, 10, 10};
+    
+    // RGP array.
+    String rpg[] = { "Skyrim", "DragonAge", "Oblivion" };
+    int rpgCopies[] = {10, 10, 10};
+    
+    // Sports array.
+    String sports[] = { "FIFA5", "Lethal League", "Starwhal" };
+    int sportsCopies[] = {10, 10, 10};
+    
+    // Strategy array.
+    String strategy[] = { "Myst", "The Room", "Darkest Dungeon" };
+    int stratCopies[] = {10, 10, 10};
+    
+    // Indie array. 
+    String indie[] = { "Runes of Brennos", "Ceres", "Undertales" };
+    int indieCopies[] = {10, 10, 10};
+    
+    // Massively Multiplayer array.
+    String multiMass[] = { "Halo", "Destiny", "Grand Theft Auto Online" };
+    int multiCopies[] = {10, 10, 10};
+    
+    // Casual array.
+    String casual[] = { "Minecraft", "Besiege", "Titan Souls" };
+    int casualCopies[] = {10, 10, 10};
+    
+    // Simulation array.
+    String simulation[] = { "Dungeon 2", "Piloteer", "Creativerse" };
+    int simCopies[] = {10, 10, 10};
+    
+    // Racing array.
+    String racing[] = { "Wipeout", "Midtown Madness 3", "DiRT" };
+    int raceCopies[] = {10, 10, 10};
+    
+
+
     //--- C O N S T R U C T O R S ----------------//-----------
     
     public Game()
     {
-        this.setTitle(title);
-        this.setGenre(genre);
-        this.setQuantity(quantity);
-        this.setId_Num(id_Num);
+        title = null;
+        genre = null;
+        quantity = 0;
+        id_Num = 0;
     }
     
     public Game(String t, String g, int q, int i)
